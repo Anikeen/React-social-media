7c9845092822a252d981afc181aefc7f3a461821
Petr Anikin <p.anikien@gmail.com>
2021-04-26 12:46:21 +0300

    Creating homepage.

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -2,14 +2,16 @@ import {Route, Switch} from 'react-router-dom';
 import './Main.css';
 import Header from '../../Header/Header'
 import Footer from '../../Footer/Footer';
+import Home from '../../../views/Home/Home';
 
 function LayoutMain() {
   return (
     <div className="main-layout">
       <Header/>
 
-      <main className="main-content">
+      <main className="main-content container">
         <Switch>
+          <Route exact path="/" component={Home} />
         </Switch>
       </main>
       
---
+++ src/views/Home/Home.js
@@ -0,0 +1,23 @@
+import './Home.css'
+
+function Greeting() {
+  return (
+    <section className="greeting">
+      <h1 className="greeting__title">
+        свитер
+      </h1>
+
+      <p className="greeting__text">
+        Какой-то воодушевляющий текст!
+      </p>
+    </section>
+  );
+}
+
+function Home() {
+  return (
+    <Greeting />
+  );
+}
+
+export default Home;
