3779c6f652087b375085bdc4fa2f0c0cd40bb4a1
Petr Anikin <p.anikien@gmail.com>
2021-09-20 15:39:05 +0300

    [Refactor] Renamed store and root reduser files to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import Store from './store/store';
+import Store from './store/index';
 // import reportWebVitals from './reportWebVitals';
 
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from  './reducers/root';
+import rootReducer from  './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const Store = createStore(combineReducers(rootReducer), composeWithDevTools(applyMiddleware(thunk)));
