2884735730471076bc17922c839745e615cc0baa
Petr Anikin <p.anikien@gmail.com>
2021-09-22 15:57:08 +0300

    [Feat] Added default user.

--- src/api/user/login.js
+++ src/api/user/login.js
@@ -1,5 +1,13 @@
 
 function loginUser(username, password) {    // this function emits a request to the server
+  if (username === '1@1.ru' && password == 123456) { // default user
+    return {
+      id: 1,
+      name: 'User',
+      secondName: 'Test',
+      image: null
+    };
+  }
   const storage = localStorage.getItem('users').split('|');
   const users = storage.map(user => JSON.parse(user));
   const user = users.find(user => user.username === username && user.password === password);
