c5455da9d5c86c99842e2d0d22fb6511fb7ecfc9
Petr Anikin <p.anikien@gmail.com>
2021-09-28 21:41:38 +0300

    [Feat] Created dropdown menu.

---
+++ src/components/UI/Dropdown/Dropdown.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './dropdown.scss';
+
+function Dropdown(props) {
+  const [isShown, setIsShown] = useState(false);
+
+  function toggleDrop() {
+    setIsShown(!isShown);
+  }
+
+  return (
+    <div className="dropdown">
+      <button className="dropdown__button" onClick={toggleDrop}>
+        {props.dropdownButton}
+      </button>
+
+      {
+        isShown
+        ?
+        <div className="dropdown__menu">
+          {props.children}
+        </div>
+        :
+        ''
+      }
+    </div>
+  )
+}
+
+export default Dropdown;
