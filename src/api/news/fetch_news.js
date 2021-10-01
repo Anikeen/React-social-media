97220c8176a0e7a22ce93758b0fd48227ed1d7fa
Petr Anikin <p.anikien@gmail.com>
2021-10-01 14:01:11 +0300

    [Feat] News post full date added.

--- src/api/news/fetch_news.js
+++ src/api/news/fetch_news.js
@@ -1,18 +1,26 @@
+function getDate() {
+  const date = new Date();
+  const fullDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`;
+  const fullTime = `${date. getHours()}:${date. getMinutes()}:${date. getSeconds()}`;
+
+  return fullDate + ' ' + fullTime;
+}
+
 function fetchNews() {
   const news = [
     {
       author: 'User One',
-      date: new Date().getDate(),
+      date: getDate(),
       text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, inventore suscipit! Placeat atque veniam non ad, sequi quasi ipsa iure unde dolore eos, ipsum laudantium sint voluptatum odio dolorem odit?'
     },
     {
       author: 'User Two',
-      date: new Date().getDate(),
+      date: getDate(),
       text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, inventore suscipit! Placeat atque veniam non ad, sequi quasi ipsa iure unde dolore eos, ipsum laudantium sint voluptatum odio dolorem odit?'
     },
     {
       author: 'User Three',
-      date: new Date().getDate(),
+      date: getDate(),
       text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, inventore suscipit! Placeat atque veniam non ad, sequi quasi ipsa iure unde dolore eos, ipsum laudantium sint voluptatum odio dolorem odit?'
     }
   ];
