e1340704287c033eb63af6d979714aaac1f67023
Petr Anikin <p.anikien@gmail.com>
2021-10-06 13:45:45 +0300

    [Feat] Created user info api.

---
+++ src/api/user/fetch_user_info.js
@@ -0,0 +1,14 @@
+
+function fetchUserInfo(id) {
+  return {
+    basic: {
+      birthday: '1 января 1970 г.',
+      city: 'Бобруйск'
+    },
+    additional: {
+
+    }
+  }
+}
+
+export default fetchUserInfo;
