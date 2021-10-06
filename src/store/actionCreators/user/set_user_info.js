76768b7bcd724bd4a2276a4b1fddff7a21f92695
Petr Anikin <p.anikien@gmail.com>
2021-10-06 16:56:15 +0300

    [Fix] Wrong import path.

--- src/store/actionCreators/user/set_user_info.js
+++ src/store/actionCreators/user/set_user_info.js
@@ -1,4 +1,4 @@
-import SET_USER_INFO from '../../actions/news/set_user_info';
+import SET_USER_INFO from '../../actions/user/set_user_info';
 
 function setUserInfo(user) {
   return {type: SET_USER_INFO, payload: user}
