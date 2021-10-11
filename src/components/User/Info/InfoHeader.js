04fb29ed1996537929045c3b5594ffe220de5171
Petr Anikin <p.anikien@gmail.com>
2021-10-11 12:13:18 +0300

    [Refactor] Changed class names.

--- src/components/User/Info/InfoHeader.js
+++ src/components/User/Info/InfoHeader.js
@@ -2,9 +2,9 @@
 function UserInfoHeader(props) {
   return (
     <div className="user-info-header">
-      <h1 className="user-info__username">{props.info.name + ' ' + props.info.secondName}</h1>
-      <div className="user-info__online">{props.info.online}</div>
-      <div className="user-info__status">{props.info.status}</div>
+      <h1 className="user-info-header__username">{props.info.name + ' ' + props.info.secondName}</h1>
+      <div className="user-info-header__online">{props.info.online}</div>
+      <div className="user-info-header__status">{props.info.status}</div>
     </div>
   );
 }
