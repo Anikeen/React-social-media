b0cebc3bdd7cd7d8ee36b4ad368be04cfefd534f
Petr Anikin <p.anikien@gmail.com>
2021-10-11 12:06:03 +0300

    [Feat] Added new user info.

--- src/api/user/fetch_user_info.js
+++ src/api/user/fetch_user_info.js
@@ -1,13 +1,18 @@
 
 function fetchUserInfo(id) {
   return {
+    user: {
+      id: 1,
+      name: 'User',
+      secondName: 'Test',
+      online: 'online',
+      status: 'Всем привет!'
+    },
     basic: {
       birthday: '1 января 1970 г.',
       city: 'Бобруйск'
     },
-    additional: {
-
-    }
+    additional: null
   }
 }
 
