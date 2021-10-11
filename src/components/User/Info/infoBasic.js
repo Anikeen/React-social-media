ff9508287010b6472ca129b70f1b7863aa1e21d4
Petr Anikin <p.anikien@gmail.com>
2021-10-11 13:21:06 +0300

    [Fix] Wrong property name.

--- src/components/User/Info/infoBasic.js
+++ src/components/User/Info/infoBasic.js
@@ -9,8 +9,8 @@ function UserInfoBasic(props) {
         info
         ?
           <div className="user-info-basic__items">
-            {info.birthday && <UserInfoItem title="День рождения: " text={info.birthday} /> }
-            {info.city     && <UserInfoItem title="Город: " text={info.city} /> }
+            {info.birthday && <UserInfoItem label="День рождения: " text={info.birthday} /> }
+            {info.city     && <UserInfoItem label="Город: " text={info.city} /> }
           </div>
         :
           <div className="user-info-body__missing">
