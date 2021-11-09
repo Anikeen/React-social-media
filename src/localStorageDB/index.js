9742c4e3140b50fc9fac9fe6e1c644d4571389e2
Anikeen <p.anikeen@gmail.com>
2021-11-09 15:11:09 +0200

    [Feat] Added a class that works with local storage. It simulates a database.

---
+++ src/localStorageDB/index.js
@@ -0,0 +1,39 @@
+class DB {
+  constructor(key) {
+    this.key = key;
+  }
+
+  getAll() {
+    let storage = localStorage.getItem(this.key);
+    if (storage) storage = storage.split('|').map(item => JSON.parse(item));
+    return storage;
+  }
+
+  getByValue(prop, value) {
+    let items = this.getAll();
+    if (items) items = items.filter(item => item[prop] == value);
+    if (items.length <= 0) items = null;
+    return items;
+  }
+
+  getSingleByID(value) {
+    let item = this.getByValue('id', value);
+    if (item) item = item[0];
+    return item;
+  }
+
+  store(value) {
+    const serialized = value.map(item => JSON.stringify(item)).join('|');
+    localStorage.setItem(this.key, serialized);
+  }
+
+  dropTable() {
+    localStorage.removeItem(this.key);
+  }
+
+  dropAll() {
+    localStorage.clear();
+  }
+}
+
+export default DB;
